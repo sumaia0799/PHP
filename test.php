0c2e9c566ef3d7ee32ca25871b8455a61a3186b4
sumaia0799 <9024814@student.zadkine.nl>
2025-12-03 11:14:00 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+// Echo voorbeeld
+echo "<h1>Welkom bij mijn PHP pagina</h1>";
+echo "<p>Dit is een voorbeeld van een paragraaf.</p>";
+
+// Variabele voorbeeld
+$naam = "wereld";
+echo "<p>Hallo $naam!</p>";
+?>
+
